a3e397643e64486c1ee05d10029c8053b10466c0
Matias Muhonen <mmu@iki.fi>
2013-08-17 19:14:22 +0300

    Add an assertion.

--- Raamattu/MABibleSearchViewController.m
+++ Raamattu/MABibleSearchViewController.m
@@ -29,6 +29,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    NSAssert(self.bibleNavigationController, @"Bible navigation controller cannot be nil");
 }
 
 - (void)didReceiveMemoryWarning
